255e4da2e72c963249886fc78365ceddb4ed3b7c
Josh Jameson <jj@xail.me>
2026-02-26 17:42:36 +0000

    working prototype
    Initialize U8g2 library and update rain detection logic.

--- project1.ino
+++ project1.ino
@@ -6,6 +6,47 @@
 */
 #include <math.h>
 
+/*
+  Universal 8bit Graphics Library (https://github.com/olikraus/u8g2/)
+  Copyright (c) 2016, olikraus@gmail.com
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without modification, 
+  are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice, this list 
+    of conditions and the following disclaimer.
+    
+  * Redistributions in binary form must reproduce the above copyright notice, this 
+    list of conditions and the following disclaimer in the documentation and/or other 
+    materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
+*/
+#include <Arduino.h>
+#include <U8g2lib.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
 const int B = 3950;            // B value of the thermistor
 const int R0 = 100000;   
 const int pinRainSensor = A3;
@@ -26,12 +67,13 @@ void setup() {
   pinMode(pinButton, INPUT);
   pinMode(pinLED, OUTPUT);
 
-  // initiate the temp sensor
+  u8g2.begin();
 
   Serial.println("Setup finished");
 }
 
 int rainVal, tempVal, lightVal;
+bool isRaining = false;
 void loop() {
   // read the rain sensor
   rainVal = analogRead(pinRainSensor);
@@ -59,13 +101,43 @@ void loop() {
     Serial.println("BUTTON Pressed!");
   }
 
-  // Test: Set the LED on
-  if (lightVal>200) {
+  // is it raining?
+  if (rainVal<75) {
+    isRaining = true;
+  } else {
+    isRaining = false;
+  }
+
+  if (isRaining) {
+    // turn LED on
     digitalWrite(pinLED, HIGH);
   } else {
+    // turn LED off
     digitalWrite(pinLED, LOW);
   }
 
   delay(500);
   Serial.println("=================");
+
+  u8g2.clearBuffer();					// clear the internal memory
+  u8g2.setFont(u8g2_font_ncenB08_tr);	// choose a suitable font
+  u8g2.drawStr(0,10,"Temp: ");	// write something to the internal memory
+
+  // convert temp float to char[]
+  char buf[16];  // big enough for "-123.45\0" etc.
+  dtostrf(temperature, 0, 1, buf);  // width 0, 1 decimal place
+
+  u8g2.drawStr(40,10,buf);	// write something to the internal memory
+  u8g2.drawStr(60,10,"C"); // temp unit
+
+
+  // say if it is raining or not
+  if (isRaining) {
+    u8g2.drawStr(0,40,"It is raining :-(");
+  } else {
+    u8g2.drawStr(0,40,"It is not raining :-)");
+  }
+
+  u8g2.sendBuffer();					// transfer internal memory to the display
+  delay(1000);  
 }
