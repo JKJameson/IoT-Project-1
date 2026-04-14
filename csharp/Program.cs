ef1111b96c227da64d72b0ce0a26a76b32026316
AhmadATU <S00287223@atu.ie>
2026-04-14 14:52:24 +0100

    Humidity next line

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -30,10 +30,10 @@ class Program {
         Console.WriteLine("Setting base frame...");
         display.DisplayBase();
 
-        const ushort line3X = 4, line3Y = 58, screenW = 244, screenH = 14;
+        const ushort line3X = 4, line3Y = 58,line4X = 5,line4Y = 68, screenW = 244, screenH = 14;
         Dht11.Reading dht11Reading;
         float tempC, humidity;
-        string line3;
+        string line3, line4;
 
         while (true)
         {
@@ -43,12 +43,14 @@ class Program {
                 tempC = dht11Reading.TemperatureC;
                 humidity = dht11Reading.Humidity;
                 
-                line3 = $"{tempC:F1}C {humidity:F0}% RH";
+                line3 = $"{tempC}C";
+                line4 = $"{humidity}% RH";
             }
             catch (Exception e)
             {
                 Console.Error.WriteLine($"DHT11: {e.Message}");
                 line3 = "-- sensor error --";
+                line4 = "-- sensor error --";
             }
 
             display.ClearWindow(24, 6, 24 + screenW, 6 + 16, WHITE);
@@ -56,10 +58,14 @@ class Program {
 
             display.ClearWindow(line3X, line3Y, line3X + screenW, line3Y + screenH, WHITE);
             display.DrawText(line3X, line3Y, line3, Font.F12, BLACK, WHITE);
+            display.ClearWindow(line4X, line4Y, line4X + screenW, line4Y + screenH, WHITE);
+            display.DrawText(line4X, line4Y, line4, Font.F12, BLACK, WHITE);
+            
 
             display.DisplayPartial();
 
             Console.WriteLine(line3);
+            Console.WriteLine(line4);
             
             // sleep for 1 second
             Thread.Sleep(TimeSpan.FromSeconds(1));
